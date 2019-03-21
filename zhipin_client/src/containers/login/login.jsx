63874951fd2878730bf8a533978a485d097bd156
wovert <67668283@qq.com>
2019-03-21 09:54:05 +0800

    client: login/register

--- zhipin_client/src/containers/login/login.jsx
+++ zhipin_client/src/containers/login/login.jsx
@@ -8,14 +8,33 @@ import {
   List,
   InputItem,
   WhiteSpace,
-  Radio,
   Button
 } from 'antd-mobile'
 import Logo from '../../components/logo/logo'
 
 const ListItem = List.Item
 
-export default class Register extends Component {
+export default class Login extends Component {
+  // 表单收集数据
+  state = {
+    username: '', // 用户名
+    password: '', // 密码
+  }
+  login = () => {
+    console.log(this.state)
+  }
+
+  toRegister = () => {
+    this.props.history.replace('/register')
+  }
+  // 处理输入输的的改变：更新对应的状态
+  handleChange = (filed, value) => {
+
+    // 更新状态
+    this.setState({
+      [filed]: value // 属性名不是field，是filed变量的值
+    })
+  }
   render () {
     return (
       <div>
@@ -26,14 +45,15 @@ export default class Register extends Component {
             <WhiteSpace/>
             <InputItem 
               placeholder="请输入用户名"
+              onChange={val => {this.handleChange('username', val)}}
             >用户名</InputItem>
-            <WhiteSpace/>            
-            <InputItem 
+            <WhiteSpace/>            <InputItem 
               placeholder="请输入密码"
               type="password"
+              onChange={val => {this.handleChange('password', val)}}
             >密码</InputItem>
-            <Button type='primary'>登&nbsp;录</Button>
-            <Button>还没有账户</Button>
+            <Button type='primary' onClick={this.login}>登&nbsp;录</Button>
+            <Button onClick={this.toRegister}>还没有账户</Button>
           </List>
         </WingBlank>
       </div>
--- zhipin_client/src/containers/register/register.jsx
+++ zhipin_client/src/containers/register/register.jsx
@@ -16,6 +16,28 @@ import Logo from '../../components/logo/logo'
 const ListItem = List.Item
 
 export default class Register extends Component {
+  // 表单收集数据
+  state = {
+    username: '', // 用户名
+    password: '', // 密码
+    password2: '', // 确认密码
+    type: '', // 用户类型 大神|老板
+  }
+  register = () => {
+    console.log(this.state)
+  }
+
+  toLogin = () => {
+    this.props.history.replace('/login')
+  }
+  // 处理输入输的的改变：更新对应的状态
+  handleChange = (filed, value) => {
+
+    // 更新状态
+    this.setState({
+      [filed]: value // 属性名不是field，是filed变量的值
+    })
+  }
   render () {
     return (
       <div>
@@ -26,26 +48,35 @@ export default class Register extends Component {
             <WhiteSpace/>
             <InputItem 
               placeholder="请输入用户名"
+              onChange={val => {this.handleChange('username', val)}}
             >用户名</InputItem>
             <WhiteSpace/>            <InputItem 
               placeholder="请输入密码"
               type="password"
+              onChange={val => {this.handleChange('password', val)}}
             >密码</InputItem>
             <WhiteSpace/>
             <InputItem 
               placeholder="请输入确认密码"
               type="password"
+              onChange={val => {this.handleChange('password2', val)}}
             >确认密码</InputItem>
             <WhiteSpace/>
             <ListItem>
               <span>用户类型</span>
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <Radio>大神</Radio>
+              <Radio
+                checked={this.state.type === 'dashen'}
+                onChange={val => {this.handleChange('type', 'dashen')}}
+              >大神</Radio>
               &nbsp;&nbsp;&nbsp;&nbsp;
-              <Radio>老板</Radio>
+              <Radio
+                checked={this.state.type === 'laoban'}
+                onChange={val => {this.handleChange('type', 'laoban')}}
+              >老板</Radio>
             </ListItem>
-            <Button type='primary'>注&nbsp;册</Button>
-            <Button>已有账户</Button>
+            <Button type='primary' onClick={this.register}>注&nbsp;册</Button>
+            <Button onClick={this.toLogin}>已有账户</Button>
           </List>
         </WingBlank>
       </div>
